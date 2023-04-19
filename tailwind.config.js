076f5706ba785bda85a00a3752b35fd5dfe50f26
MySersan <mysersan.tn@gmail.com>
2023-04-19 16:28:12 +0700

    setup flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,25 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+content: [
+'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+'./vendor/laravel/jetstream/**/*.blade.php',
+'./storage/framework/views/*.php',
+'./resources/views/**/*.blade.php',
+'./node_modules/flowbite/**/*.js',
+],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+theme: {
+extend: {
+fontFamily: {
+sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+},
+},
+},
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('flowbite/plugin')
+],
 };
