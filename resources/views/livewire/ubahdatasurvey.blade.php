2748443f2d368ea744770ee1f836bf6ac952f334
endah12 <endahnurcayanti56@gmail.com>
2023-05-19 14:16:12 +0700

    form ubah data survey dan update button

--- resources/views/livewire/ubahdatasurvey.blade.php
+++ resources/views/livewire/ubahdatasurvey.blade.php
@@ -1,7 +1,7 @@
 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
     <div class="p-6 lg:p-8 bg-white border-b border-gray-200 rounded-xl mt-8">
         <h1 class="text-2xl font-medium text-gray-900">
-           Ubah Data Nama dan Alamat
+           Ubah Data Survey
         </h1>
         <hr class="mb-6">
         <form>
@@ -45,7 +45,7 @@
             </div>
             <div class="mb-6">
                 <label for="nama-pemohon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
-                    Pemohon
+                    Pemilik
                     *</label>
                 <input type="text" id="nama-pemohon"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -53,38 +53,56 @@
             </div>
             <div class="mb-6">
                 <label for="alamat-pemohon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat
-                    Pemohon *</label>
+                    Pemasangan *</label>
                 <textarea id="alamat-pemohon" rows="4"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Tulis alamat lengkap pemohon..."></textarea>
             </div>
             <div class="mb-6">
-                <label for="nama-pemilik" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
-                    Calon Pelanggan
-                    *</label>
-                <input type="text" id="nama-pemilik"
+                <label for="sambungan-baru" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Wilayah</label>
+                <select id="sambungan-baru"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </select>
+            </div>
+            <div class="mb-6">
+                <label for="sambungan-baru" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jalan</label>
+                <select id="sambungan-baru"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </select>
+            </div>
+            <div class="mb-6">
+                <label for="sambungan-baru" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Penggunaan Air Bersih</label>
+                <select id="sambungan-baru"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </select>
+            </div>
+            <div class="mb-6">
+                <label for="sambungan-baru" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Golongan Tarif</label>
+                <select id="sambungan-baru"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </select>
+            </div>
+            <div class="mb-6">
+                <label for="sambungan-baru" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status Survey</label>
+                <select id="sambungan-baru"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </select>
+            </div>
+            <div class="mb-6">
+                <label for="nama-pemohon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NPA Lama</label>
+                <input type="text" id="nama-pemohon"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="" required>
             </div>
             <div class="mb-6">
-                <label for="alamat-pemasangan"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat Pemasangan
-                    *</label>
-                <input type="text" id="alamat-pemasangan"
+                <label for="nama-pemohon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tunggakan</label>
+                <input type="text" id="nama-pemohon"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="" required>
             </div>
             <div class="mb-6">
-                <label for="no-rumah" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">No
-                    *</label>
-                <input type="text" id="no-rumah"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="" required>
-            </div>
-            <div class="mb-6">
-                <label for="no-rumah" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">No Hp Calon Pelanggan
-                    *</label>
-                <input type="text" id="no-rumah"
+                <label for="nama-pemohon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kasus</label>
+                <input type="text" id="nama-pemohon"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="" required>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Livewire\{PendaftaranPelanggan,Ubahdatanama};
+use App\Http\Livewire\{PendaftaranPelanggan,Ubahdatanama,Ubahdatasurvey};
 use App\Http\Livewire\{PendaftaranMain,PembayaranMain,PusatMain,SuperUserMain};
 use App\Http\Livewire\{MasterCabang,MasterGolonganBangunan,MasterJalan,MasterKecamatan,MasterKelurahan,MasterKodePos,MasterKotaKabupaten,MasterMaterial,MasterProvinsi,MasterRekanan,MasterTypeMeter,MasterWilayah};
 
@@ -35,7 +35,7 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
     // submenu
     Route::get('/pendaftaran-pelanggan', PendaftaranPelanggan::class)->name('pendaftaran-pelanggan');
     Route::get('/ubahdatanama', Ubahdatanama::class)->name('ubahdatanama');
-
+    Route::get('/ubahdatasurvey', Ubahdatasurvey::class)->name('ubahdatasurvey');
 
     // Submenu Master Data
     Route::get('/master-cabang', MasterCabang::class)->name('master-cabang');
