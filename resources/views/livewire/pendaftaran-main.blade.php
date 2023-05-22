8b7e7a8ae05ad483296e2a6ec6656831d8135aed
endah12 <endahnurcayanti56@gmail.com>
2023-05-22 11:26:02 +0700

    form cetak ulang pemberitahuan biaya pasang

--- resources/views/livewire/pendaftaran-main.blade.php
+++ resources/views/livewire/pendaftaran-main.blade.php
@@ -248,7 +248,7 @@
                                         Cetak Ulang Pemberitahuan Biaya Pasang
                                     </th>
                                     <td class="px-6 py-4 text-left">
-                                        <a href="pendaftaran-pelanggan"
+                                        <a href="pemberitahuanbiayapasang"
                                             class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Buka</a>
                                     </td>
                                 </tr>
