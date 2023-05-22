fc0f24d30a39e8293280af08fbd7a61e7ed9be7d
endah12 <endahnurcayanti56@gmail.com>
2023-05-22 12:20:26 +0700

    form cetak ulang bppb

--- resources/views/livewire/pendaftaran-main.blade.php
+++ resources/views/livewire/pendaftaran-main.blade.php
@@ -281,7 +281,7 @@
                                         Cetak Ulang BPPB
                                     </th>
                                     <td class="px-6 py-4 text-left">
-                                        <a href="pendaftaran-pelanggan"
+                                        <a href="cetakulangbppb"
                                             class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Buka</a>
                                     </td>
                                 </tr>
