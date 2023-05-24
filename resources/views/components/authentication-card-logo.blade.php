29383ed29674e69bb62a0f0245dd94cebb72a5f9
MySersan <mysersan.tn@gmail.com>
2023-05-24 15:42:45 +0700

    update login form

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,7 @@
-<a href="/">
-    <img src="{{ asset('/images/tirtanadi-logo.png') }}" alt="tirtanadi-logo" style="width: 96px;">
-</a>
+
+<div class="grid grid-rows-1 justify-items-center">
+    <a href="/">
+        <img src="{{ asset('/images/tirtanadi-logo.png') }}" alt="tirtanadi-logo" style="width: 96px;">
+    </a>
+    <div class="text-center text-gray-500 text-2xl font-extrabold mt-2">PASANG BARU PERUMDA TIRTANADI</div>
+</div>
